47e10b2f39334891af2c18538c6fcd02512ccf85
akenworthy20 <37529573+akenworthy20@users.noreply.github.com>
2018-04-25 20:57:34 -0700

    Create dbconn.php

---
+++ dbconn.php
@@ -0,0 +1,16 @@
+<?php
+  function connect_to_db( $dbname ){
+	  $dbc = @mysqli_connect( "localhost", "root", "root", $dbname ) or
+		die( "Connect failed: ". mysqli_connect_error() );
+	  return $dbc;
+  }
+  function disconnect_from_db( $dbc, $result ){
+	  mysqli_free_result( $result );
+	  mysqli_close( $dbc );
+  }
+  function perform_query( $dbc, $query ){
+	  $result = mysqli_query($dbc, $query) or
+		die( "bad query".mysqli_error( $dbc ) );
+	  return $result;
+  }
+?>
