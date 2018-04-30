7ee25507510316c5541cefbafdac17c460ab3586
akenworthy20 <37529573+akenworthy20@users.noreply.github.com>
2018-04-30 00:00:49 -0700

    Add files via upload

--- routes.php
+++ routes.php
@@ -3,19 +3,19 @@
     //require the file that controller requests
     require_once('controller/' . $controller . '_controller.php');
 
-  //create a new instance of a controller
-  switch($controller) {
-    case 'pages':
-      $controller = new PagesController();
-    break;
-  }
+    //create a new instance of a controller
+    switch($controller) {
+      case 'pages':
+        $controller = new PagesController();
+      break;
+    }
 
-  //call the action
-  $controller->{ action }();
+    //call the action
+    $controller->{ $action }();
   }
 
   //create a list of all the controllers that are defined
-  $controllers = array('pages' => ['home','error']);
+  $controllers = array('pages' => ['home','aboutUs','studentRegister','tutorRegister','error']);
 
   //check to see if requested controller is in $controllers
   if (array_key_exists($controller, $controllers)) {
