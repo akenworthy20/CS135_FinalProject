709b911dfdbed63e1ee8f4530936b7ee6ae844ba
akenworthy20 <37529573+akenworthy20@users.noreply.github.com>
2018-04-30 00:02:56 -0700

    Add files via upload

--- view/pages/home.php
+++ view/pages/home.php
@@ -1,1 +1,10 @@
-<p> this is our home page </p>
+<h1> Login </h1>
+<form name="loginForm" method="post">
+
+  <legend for="username">Username:
+  <input type="text" name="username" id ="username" value=""> </input></legend>
+
+  <legend for="password">Password:
+  <input type="password" name="password" id ="password"> </input></legend>
+
+</form>
