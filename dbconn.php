5c8d3a36a05186aaceaefd843408736056bfe3bf
chogan97 <38477400+chogan97@users.noreply.github.com>
2018-04-29 20:22:14 -0700

    Update dbconn.php

--- dbconn.php
+++ dbconn.php
@@ -8,10 +8,10 @@
 
     public static function getInstance() {
         if (!isset(self::$instance)) {
-          $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRORMODE_EXCEPTION;
-          self::instance = new PDO('mysql:host=localhost;dbname=tm_ddl','root','root', $pdo_options);
+          $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+          self::$instance = new PDO('mysql:host=localhost;dbname=tm_ddl','root','root', $pdo_options);
         }
-        return self::instance;
+        return self::$instance;
     }
   }
 ?>
