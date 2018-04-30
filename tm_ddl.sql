1a21cecc9d0ee6f712c232339fa504506b456bfe
akenworthy20 <37529573+akenworthy20@users.noreply.github.com>
2018-04-30 09:27:58 -0700

    Update tm_ddl.sql

--- tm_ddl.sql
+++ tm_ddl.sql
@@ -10,7 +10,7 @@ CREATE TABLE Student {
   classes_seeking VARCHAR (256) NOT NULL,
   available_day INT UNSIGNED NOT NULL, 
   available_time TIME NOT NULL,
-  grad_year INT UNSIGNED NOT NULL, 
+  grad_year INT UNSIGNED NOT NULL
 }
 
 /*Add deposit info to tutor table*/
@@ -19,27 +19,24 @@ CREATE TABLE Tutor {
   first_name VARCHAR (256) NOT NULL,  
   last_name VARCHAR (256) NOT NULL, 
   phone_number VARCHAR (256) NOT NULL, 
-  available_day INT UNSIGNED NOT NULL. 
+  available_day INT UNSIGNED NOT NULL,
   available_time TIME NOT NULL,
   grad_year INT UNSIGNED NOT NULL, 
   major VARCHAR (256) NOT NULL 
 }
+
+CREATE TABLE Availability {
+  tutor_id INT UNSIGNED NOT NULL FOREIGN KEY REFERENCES TUTOR(id),
+  student_id INT UNSIGNED NOT NULL FOREIGN KEY REFERENCES STUDENT(id)
+}
+
 
 /*Note - removed textbook data element from classes*/
 CREATE TABLE Classes {
   id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, 
   tutor_id INT UNSIGNED NOT NULL FOREIGN KEY REFERENCES TUTOR(id),
   professor VARCHAR (256) NOT NULL,
-  subject VARCHAR (256) NOT NULL
+  subject VARCHAR (256) NOT NULL,
   grade_received VARCHAR (256) NOT NULL,  
   school_taken VARCHAR (256) NOT NULL
-}
-
-CREATE TABLE Appointments {
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-  class_id INT UNSIGNED NOT NULL FOREIGN KEY REFERENCES Classes(id), 
-  tutor_id INT UNSIGNED NOT NULL FOREIGN KEY REFERENCES Tutor(id), 
-  student_id INT UNSIGNED NOT NULL FOREIGN KEY REFERENCES Student(id), 
-  appt_date DATE NOT NULL, 
-  appt_time TIME NOT NULL
 }
