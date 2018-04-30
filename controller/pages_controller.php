de9bd6c84de38ad094a5138134d5fcd0768ca4e2
akenworthy20 <37529573+akenworthy20@users.noreply.github.com>
2018-04-30 00:01:35 -0700

    Add files via upload

--- controller/pages_controller.php
+++ controller/pages_controller.php
@@ -1,12 +1,18 @@
 <?php
-	class PagesController { 
-		public function home() { 
+	class PagesController {
+		public function home() {
 			require_once('view/pages/home.php');
 		}
-		public function register() { 
-			require_once('view/pages/registration.php');
+		public function aboutUs() {
+			require_once('view/pages/aboutUs.php');
 		}
-		public function error() { 
+		public function studentRegister() {
+			require_once('view/pages/studentRegister.php');
+		}
+		public function tutorRegister() {
+			require_once('view/pages/tutorRegister.php');
+		}
+		public function error() {
 			require_once('view/pages/error.php');
 		}
 	}
