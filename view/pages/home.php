27afd0eff86c874b9bada8a20a872fcfa4615b7b
Avi Vemuri <avemuri19@cmc.edu>
2018-04-30 23:41:10 -0700

    Update home.php

--- view/pages/home.php
+++ view/pages/home.php
@@ -29,7 +29,7 @@
             <div style = "margin:30px">
 
                <form action = "" method = "post">
-                  <label>Username  :</label><input type = "text" name = "username" class = "box"/><br /><br />
+                  <label>Email Address: </label><input type = "text" name = "username" class = "box"/><br /><br />
                   <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
                   <input type = "submit" value = " Submit "/><br />
                </form>
