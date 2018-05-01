92c919da6e20212184fac62715a4336e76c30a20
Avi Vemuri <avemuri19@cmc.edu>
2018-04-30 18:43:17 -0700

    Update loginCheck.php

--- model/loginCheck.php
+++ model/loginCheck.php
@@ -1,65 +1,31 @@
 <?php
+   include("dbconn.php");
+   session_start();
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+      $db = db::getInstance();
+      $username = mysqli_real_escape_string($db,$_POST['username']);
+      $password = mysqli_real_escape_string($db,$_POST['password']);
+      $error =  "";
 
-require_once('dbconn.php');
-
-// Define variables and initialize with empty values
-$username = $password = "";
-$username_err = $password_err = "";
- 
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
- // Check usernme present
-    if(empty(trim($_POST["username"]))){
-       $username_err = 'Please enter username.';
-    } else{
-       $username = trim($_POST["username"]);
-    }
-    
-    // Check password present
-    if(empty(trim($_POST['password']))){
-        $password_err = 'Please enter your password.';
-    } else{
-        $password = trim($_POST['password']);
+      
+      $sql = "SELECT id FROM students WHERE username = '$username' and password = '$password'";
+      $result = mysqli_query($db,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      $active = $row['active'];
+      
+      $count = mysqli_num_rows($result);
+      
+      // If result matched $myusername and $mypassword, table row must be 1 row
+		
+      if($count == 1) {
+         session_register("username");
+         $_SESSION['login_user'] = $username;
+         
+         header("location: welcome.php");
+      }else {
+         $error = "Your Login Name or Password is invalid";
       }
-}
-
-if(empty($username_err) && empty($password_err)) { 
-	$sql = "SELECT username, password FROM students WHERE username = ?";
-
-	if($stmt = mysqli_prepare($link, $sql)) {
-
-		mysqli_stmt_bind_param($stmt,"s", $param,_username);
-
-		//Set parameters
-		$param_username = $username; 
-
-		if(mysqli_stmt_execute($stmt)) {
-			mysqli_stmt_store_result($stmt);
-
-			if(mysqli_stmt_num_rows($stmt) == 1) {
-				mysqli_stmt_bind_result($stmt, $username, $hashed_password);
-
-				if(mysqli_stmt_fetch($stmt)){
-                        if(password_verify($password, $hashed_password)){
-
-                        	session_start();
-                            $_SESSION['username'] = $username;      
-                            header("location: welcome.php");
-                        } else {
-                        	 $password_err = 'The password you entered was not valid.';
-                        }
-            	}
-            } else {
-            	 $username_err = 'No account found with that username.';
-              }
- 			} else {
- 				echo "Ooops! Something went wrong. Please try again later.";
- 			}            
-
-        }
-
-        mysqli_stmt_close($stmt);
-	}
-	mysqli_close($link);
-}
+   }
 ?>
