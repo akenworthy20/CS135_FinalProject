606f9b9cdc913bb543c33b9f06d443a91f4f8adf
Avi Vemuri <avemuri19@cmc.edu>
2018-04-30 17:55:04 -0700

    Update home.php

--- view/pages/home.php
+++ view/pages/home.php
@@ -1,10 +1,31 @@
-<h1> Login </h1>
-<form name="loginForm" method="post">
-
-  <legend for="username">Username:
-  <input type="text" name="username" id ="username" value=""> </input></legend>
-
-  <legend for="password">Password:
-  <input type="password" name="password" id ="password"> </input></legend>
-
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Login</title>
+    <style>
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Login</h2>
+        <p>Please fill in your credentials to login.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <label>Username</label>
+                <input type="text" name="username"class="form-control" value="<?php echo $username; ?>">
+                <span class="help-block"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control">
+                <span class="help-block"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Login">
+            </div>
+            <p>Don't have an account? <a href="studentRegister.php">Student Registration!</a><a href = "tutorRegister.php">Tutor Registration!</a></p>
+        </form>
+    </div>    
+</body>
+</html>
