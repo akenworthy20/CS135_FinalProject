1e322fe861fb3cab5b9a9818486a93fd7b924b7a
Avi Vemuri <avemuri19@cmc.edu>
2018-05-01 00:05:50 -0700

    Update loginCheck.php

--- model/loginCheck.php
+++ model/loginCheck.php
@@ -1,29 +1,29 @@
 <?php
    include_once("dbconn.php");
    session_start();
+   
    if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form
-      $database = db::getInstance();
-      $username = mysqli_real_escape_string($database,$_POST['username']);
-      $password = mysqli_real_escape_string($database,$_POST['password']);
-      $error =  "";
+      // email and password sent from form 
+      $db = db::getInstance();
 
+      $email = mysqli_real_escape_string($db,$_POST['email']);
+      $password = mysqli_real_escape_string($db,$_POST['password']);
 
-      $sql = "SELECT id FROM students WHERE username = '$username' and password = '$password'";
-      $result = mysqli_query($database,$sql);
+      
+      $sql = "SELECT id FROM students WHERE email = '$email' and password = '$password'";
+      $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       $active = $row['active'];
-
+      
       $count = mysqli_num_rows($result);
-
-      // If result matched $myusername and $mypassword, table row must be 1 row
-
+      
+      // If result matched $email and $password, table row must be 1 row
+		
       if($count == 1) {
-         session_register("username");
-         $_SESSION['login_user'] = $username;
-
+         session_register("email");
+         $_SESSION['login_user'] = $email;         
          header("location: welcome.php");
-      }else {
+      } else {
          $error = "Your Login Name or Password is invalid";
       }
    }
