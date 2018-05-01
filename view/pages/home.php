714631218ba46cb4a925b9a00601da577c1f2e41
Avi Vemuri <avemuri19@cmc.edu>
2018-04-30 23:46:55 -0700

    Update home.php

--- view/pages/home.php
+++ view/pages/home.php
@@ -1,13 +1,12 @@
 <html>
-
+   
    <head>
       <title>Login Page</title>
-
+      
       <style type = "text/css">
          body {
             font-family:Arial, Helvetica, sans-serif;
             font-size:14px;
-            padding-left: 20px;
          }
          label {
             font-weight:bold;
@@ -18,28 +17,29 @@
             border:#666666 solid 1px;
          }
       </style>
-
+      
    </head>
-
+   
    <body bgcolor = "#FFFFFF">
-      <div align = "left">
+	
+      <div align = "center">
          <div style = "width:300px; border: solid 1px #333333; " align = "left">
             <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
-
+				
             <div style = "margin:30px">
-
+               
                <form action = "" method = "post">
-                  <label>Email Address: </label><input type = "text" name = "username" class = "box"/><br /><br />
+                  <label>UserName  :</label><input type = "text" name = "email" class = "box"/><br /><br />
                   <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
                   <input type = "submit" value = " Submit "/><br />
                </form>
-
+               
                <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
-
+					
             </div>
-
+				
          </div>
-
+			
       </div>
 
    </body>
