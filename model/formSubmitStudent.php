4462e2a66cd881b0197cc832161a89e13427d761
chogan97 <38477400+chogan97@users.noreply.github.com>
2018-05-06 16:23:15 -0700

    Add files via upload

--- model/formSubmitStudent.php
+++ model/formSubmitStudent.php
@@ -53,65 +53,68 @@ function studentSubmit($name, $email, $phone, $password, $major, $monday, $tuesd
 
  unset($pdo);
 }
+
+  $errors = 0;
   if (isset($_POST['submit'])) {
     if (empty($_POST['name'])) {
       echo "<strong> Name is required </strong><br/>";
+      $errors++;
     } else {
       $name = sanitize($_POST['name']);
       if (!preg_match("/^[a-zA-Z]*$/",$name)) {
-        echo "<strong> Please use only letters and white space </strong><br/>";
+        echo "<strong> Please use only letters and white space for name</strong><br/>";
+        $errors++;
       }
     }
     if (empty($_POST['email'])) {
       echo "<strong> E-mail is required </strong><br/>";
+      $errors++;
     } else {
       $email = sanitize($_POST['email']);
       if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         echo "<strong> Please enter a valid e-mail </strong><br/>";
+        $errors++;
       }
     }
     if (empty($_POST["phone"])) {
         echo "<strong> Please input full, valid US phone number </strong><br/>";
+        $errors++;
     } else {
       $phone = sanitize($_POST["phone"]);
       // check phone number syntax to ensure it is 10 digits long, exactly
       if (!preg_match("/[\d{3}]-?[\d{3}]-?[\d{4}]/",$phone)) {
         echo "<strong> Please input full, valid US phone number </strong><br/>";
+        $errors++;
       }
     }
     if (empty($_POST["password"])) {
         echo "<strong> Please input password </strong><br/>";
+        $errors++;
     } else {
       $password = sanitize($_POST["password"]);
       if (!preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,10}/",$password)) {
-        echo "<strong> Minimum eight and maximum 10 characters, at least one uppercase letter, one lowercase letter, one number and one special character </strong><br/>";
+        echo "<strong> Minimum eight and maximum 10 characters, at least one uppercase letter, one lowercase letter, one number and one special character for password</strong><br/>";
+        $errors++;
       }
     }
     if (empty($_POST["major"])) {
         echo "<strong> Please input a major </strong><br/>";
+        $errors++;
     } else {
       $major = sanitize($_POST["major"]);
       if (!preg_match("/^[a-zA-Z]*$/",$major)) {
-        echo "<strong> Please use only letters and white space </strong><br/>";
+        echo "<strong> Please use only letters and white space for major</strong><br/>";
+        $errors++;
       }
     }
     if(!isset($_POST["monday"]) && !isset($_POST["tuesday"]) && !isset($_POST["wednesday"]) && !isset($_POST["thursday"]) && !isset($_POST["friday"]) && !isset($_POST["saturday"]) && !isset($_POST["sunday"])){
       echo "<strong> Please select your availability </strong><br/>";
+      $errors++;
     }
-//non-required form elements from tutorRegister//
-    if (!empty($_POST["gpa"])) {
-      $gpa = sanitize($_POST["major"]);
-      if (!preg_match("/[0-4].[0-9]/",$gpa)) {
-        echo "<strong> Please round your gpa to the nearest tenth </strong><br/>";
-      }
+    if (!$errors == 0) {
+      echo "<strong> Please make sure all fields are filled correctly <strong><br/>";
+    } else {
+      studentSubmit($name, $email, $phone, $password, $major, $monday, $tuesday, $wednesday, $thursday, $friday, $saturday, $sunday);
     }
-    if (!empty($_POST["bio"])) {
-      $bio = sanitize($_POST["bio"]);
-      // check phone number syntax to ensure it is 10 digits long, exactly
-      if (!preg_match("/^[a-zA-Z]*$/",$bio)) {
-        echo "<strong> Please use only letters and white space </strong><br/>";
-      }
-    }
-    studentSubmit($name, $email, $phone, $password, $major, $monday, $tuesday, $wednesday, $thursday, $friday, $saturday, $sunday);
   }
 ?>
