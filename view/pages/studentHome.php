716b821d316120057e91767ab22cf33fa00ee1e6
akenworthy20 <37529573+akenworthy20@users.noreply.github.com>
2018-05-06 17:41:49 -0700

    Add files via upload

--- view/pages/studentHome.php
+++ view/pages/studentHome.php
@@ -6,33 +6,76 @@
 	}
 
 	$tableheader = false;
-	$query = "SELECT * FROM Tutor";
+	$query = "SELECT name,email,phone,year,major,gpa,bio,monday,tuesday,wednesday,thursday,friday,saturday,sunday FROM Tutor";
 	$sth = $dbh->prepare($query);
 
 	if(!$sth->execute()) {
 		die('Did not execute');
 	}
-
-	echo "<table>";
-
-	while($row = $sth->fetch(PDO::FETCH_ASSOC)) {
-		if($tableheader == false) {
-			echo '<tr>';
-			foreach($row as $key=>$value) {
-				echo "<th>{$key}</th>";
-			}
-			echo '</tr>';
-			$tableheader = true;
-		}
-		echo "<tr>";
-		foreach($row as $value) {
-			if ($value=="0"){
-				echo"<td>Free</td";
-			} else {
-				echo "<td>{$value}</td>";
-			}
-		}
-		echo "</tr>";
-	}
-	echo "</table>";
 ?>
+<style>
+	.zui-table {
+	    border: solid 1px #DDEEEE;
+	    border-collapse: collapse;
+	    border-spacing: 0;
+	    font: normal 13px Arial, sans-serif;
+	}
+	.zui-table thead th {
+	    background-color: #DDEFEF;
+	    border: solid 1px #DDEEEE;
+	    color: #336B6B;
+	    padding: 10px;
+	    text-align: left;
+	    text-shadow: 1px 1px 1px #fff;
+	}
+	.zui-table tbody td {
+	    border: solid 1px #DDEEEE;
+	    color: #333;
+	    padding: 10px;
+	    text-shadow: 1px 1px 1px #fff;
+	}
+</style>
+<table class = "zui-table" id = "zui-table">
+	<?php while($row = $sth->fetch(PDO::FETCH_ASSOC)) {
+		if($tableheader == false) { ?>
+			<tr>
+				<?php foreach($row as $key=>$value) { ?>
+				<th>
+					<?php
+						if ($key=="monday"){
+							echo "M";
+						} else if ($key=="tuesday"){
+							echo "T";
+						} else if ($key=="wednesday"){
+							echo "W";
+						} else if ($key=="thursday"){
+							echo "Tr";
+						} else if ($key=="friday"){
+							echo "F";
+						} else if ($key=="saturday"){
+							echo "Sat";
+						} else if ($key=="sunday"){
+							echo "Sun";
+						} else {
+							echo "{$key}";
+						}
+
+					?>
+				</th>
+				<?php } ?>
+			</tr>
+			<?php $tableheader = true;
+		} ?>
+		<tr>
+				<?php foreach($row as $value) {  ?>
+					<td>
+					<?php if ($value=="0"){
+						echo "Free";
+					} else {
+						echo "{$value}";
+					} ?>
+					</td>
+				<?php } ?>
+		</tr>
+	<?php } ?>
+</table>
