5cfa1da563f78bea2cc329428fdd9798890e179a
chogan97 <38477400+chogan97@users.noreply.github.com>
2018-05-10 15:54:40 -0700

    Add files via upload

---
+++ tutorHome.php
@@ -0,0 +1,26 @@
+  <div align = "center">
+    <h2> Tutor Home </h2>
+  </div>
+
+  <div align = "left">
+    <p> <strong> Welcome to the tutor home page! </strong> </br>
+      <?php
+      try {
+        $dbh = db::getInstance();
+      }catch(PDOException $e) {
+        die('Could not connect');
+      }
+
+      $query = "SELECT COUNT(id) as num FROM Student"
+      $sth = $dbh->prepare($query)
+      if(!$sth->execute()) {
+    		die('Did not execute');
+    	}
+
+      echo "There are currently " . 'num' . "students registered for TutorMatch!"
+      ?>
+    </p> </br> <hr>
+
+    <h3> All you have to do now is wait!  When a student wants to solicit you
+      as a tutor, they can get your contact info and will reach out, so make sure to keep an eye
+      on your phone and email! Thank you for using TutorMatch! </h3>
